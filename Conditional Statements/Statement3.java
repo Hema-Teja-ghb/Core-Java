5703bb748b23f556b3cc2f62dc7936efdb976158
Hema-Teja-ghb <tejahemateja@gmail.com>
2025-05-13 22:57:51 +0530

    Add files via upload

---
+++ Conditional Statements/Statement3.java
@@ -0,0 +1,16 @@
+package com.conditional;
+
+public class Statement3 {
+
+	public static void main(String[] args) {
+		int marks=30;
+		if (marks>=30) {
+			System.out.println("PASSED");
+		}
+		else
+		{
+			System.out.println("FAILED");
+		}
+	}
+
+}
---
+++ Conditional Statements/Statement4.java
@@ -0,0 +1,15 @@
+package com.conditional;
+
+public class Statement4 {
+
+	public static void main(String[] args) {
+		int a=9;
+		if(a%2==0) {
+			System.out.println("It is an Even Number");
+		}
+		if(a%2!=0){
+			System.out.println("It is an Odd Number");
+		}
+	}
+
+}
